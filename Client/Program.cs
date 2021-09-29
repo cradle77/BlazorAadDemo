2f920d5504ca68d67a9474ea39b6e0276206083d
MarcoDes <marcodesanctis2@gmail.com>
2021-09-29 17:50:01 +0100

    initial

--- Client/Program.cs
+++ Client/Program.cs
@@ -15,27 +15,6 @@ namespace Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
         
-            builder.Services
-                .AddHttpClient("weatherapi", client =>
-                {
-                    client.BaseAddress = new Uri("https://localhost:5002");
-                })
-                .AddHttpMessageHandler(sp =>
-                {
-                    var handler = sp.GetRequiredService<AuthorizationMessageHandler>()
-                        .ConfigureHandler(new[] { "https://localhost:5002" },
-                        scopes: new[] { "api://blazor-demo/weatherapi" });
-
-                    return handler;
-                });
-
-            builder.Services.AddSingleton(sp =>
-            {
-                var factory = sp.GetService<IHttpClientFactory>();
-
-                return factory.CreateClient("weatherapi");
-            });
-
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
