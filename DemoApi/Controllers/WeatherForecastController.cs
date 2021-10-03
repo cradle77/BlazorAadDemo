6c706e2cf40abc834e543961e6498ede6e555dd3
MarcoDes <marcodesanctis2@gmail.com>
2021-10-03 11:15:07 +0100

    user name

--- DemoApi/Controllers/WeatherForecastController.cs
+++ DemoApi/Controllers/WeatherForecastController.cs
@@ -38,7 +38,8 @@ namespace DemoApi.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                User = this.User.Identity.Name
             })
             .ToArray();
         }
